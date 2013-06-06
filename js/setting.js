9d837394ab963def7383a57c29b2dd494ebb735e
Koji Jimba <dj.jimba@gmail.com>
2013-06-07 04:35:32 +0900

    set the correct identifier

--- js/setting.js
+++ js/setting.js
@@ -10,7 +10,7 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'musashino_2012';
+OpenSpending.identifier = 'musashino_budget_2012';
 OpenSpending.year = '2012';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
